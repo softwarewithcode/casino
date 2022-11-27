db3b8316ed86c463974b7673b48bead2b1981a5b
softwarewithcode <noreply>
2022-11-27 09:20:26 +0200

    add failing junit tests

--- codes/backend/casino/blackjack/src/test/java/com/casino/blackjack/test/BlackjackPlayerTest.java
+++ codes/backend/casino/blackjack/src/test/java/com/casino/blackjack/test/BlackjackPlayerTest.java
@@ -7,24 +7,38 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.casino.blackjack.player.BlackjackPlayer;
-import com.casino.blackjack.table.BlackjackTable;
 import com.casino.common.cards.Card;
 import com.casino.common.cards.Suit;
-import com.casino.common.table.Status;
-import com.casino.common.table.Type;
 
 public class BlackjackPlayerTest {
 	@Test
-	public void userHoldsDealtCards() {
+	public void playerHoldsDealtCardsInFirstHand() {
 		BlackjackPlayer player = new BlackjackPlayer("player", UUID.randomUUID(), BigDecimal.TEN, null);
-		player.addCard(createCard(13, Suit.CLUB));
-		player.addCard(createCard(11, Suit.CLUB));
-		Assertions.assertEquals(2, player.getCards().size());
+		player.addCard(player.getHands().get(0), new Card(13, Suit.CLUB));
+		player.addCard(player.getHands().get(0), new Card(11, Suit.CLUB));
+		Assertions.assertEquals(2, player.getHands().size());
 	}
 
 	@Test
-	public void calculateSumOfBasicCards() {
-		;
+	public void handValueIsCorrect() {
+		BlackjackPlayer player = createPlayer();
+		player.addCard(player.getHands().get(0), new Card(7, Suit.CLUB));
+		player.addCard(player.getHands().get(0), new Card(8, Suit.CLUB));
+		Assertions.assertEquals(15, player.getHands().get(0).calculateValue());
+	}
+
+	@Test
+	public void pictureCardsAreValuedAsRankOfTen() {
+		BlackjackPlayer player = createPlayer();
+		player.addCard(player.getHands().get(0), new Card(12, Suit.CLUB));
+		player.addCard(player.getHands().get(0), new Card(13, Suit.CLUB));
+		Assertions.assertEquals(20, player.getHands().get(0).calculateValue());
+	}
+
+	private BlackjackPlayer createPlayer() {
+		BlackjackPlayer player = new BlackjackPlayer("player", UUID.randomUUID(), BigDecimal.TEN, null);
+
+		return player;
 	}
 
 	private Card createCard(int rank, Suit suit) {
